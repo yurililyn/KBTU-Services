0696b6250b65f80a25b111011109c056280d5866
Artem <gushinartem58@gmail.com>
2026-04-07 12:25:34 +0500

    test commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Main } from './componenets/main/main';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        component : Main
+    }
+];
