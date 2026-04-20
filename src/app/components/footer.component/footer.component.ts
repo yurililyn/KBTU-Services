3f3c0e0752aa4a8d1ea831f9ceff874e46dc7a74
Artem <gushinartem58@gmail.com>
2026-04-21 02:32:55 +0500

    fix footer

--- src/app/components/footer.component/footer.component.ts
+++ src/app/components/footer.component/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-footer',
   imports: [],
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-
+    constructor(private router : Router) {}
+    get isAuthPage(): boolean{
+    return this.router.url == '/login' || this.router.url == '/registration';
+  }
+  openTelegram(){
+    window.open("https://t.me/yurililyn");
+  }
+  openInstagram(){
+    window.open("https://www.instagram.com/konyuktor/");
+  }
 }
